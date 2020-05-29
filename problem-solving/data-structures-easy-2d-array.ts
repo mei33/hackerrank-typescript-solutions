818eb93a23f33d164a319121b1ef65aa24a14511
Maksim Andreev <andreev.kras@gmail.com>
2020-05-29 22:43:53 +0300

    Add '2D Array - DS' solution

---
+++ problem-solving/data-structures-easy-2d-array.ts
@@ -0,0 +1,24 @@
+function hourglassSum(arr: Array<number[]>): number   {
+  const sums = [];
+  const rowLength = arr[0].length;
+  const rowsAmount = arr.length;
+  const hourglassWidth = 3
+
+  for (let arrRowIdx = 0; arrRowIdx <= rowsAmount - hourglassWidth; arrRowIdx++) {
+    for (let arrInnerIdx = 0; arrInnerIdx <= rowLength - hourglassWidth; arrInnerIdx++) {
+      // top left hourglass item
+      let hourglassSum = 0;
+
+      for (let i = 0; i < hourglassWidth; i++) {
+        // first and last hourglass lines
+        hourglassSum += (arr[arrRowIdx][arrInnerIdx + i] + arr[arrRowIdx + (hourglassWidth - 1)][arrInnerIdx + i]);
+      }
+      // middle hourglass line
+      hourglassSum += arr[arrRowIdx + 1][arrInnerIdx + 1]
+
+      sums.push(hourglassSum)
+    }
+  }
+
+  return Math.max(...sums);
+}
