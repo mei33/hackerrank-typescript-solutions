ee99ef788d4df8f6d40b4d72225d58445bd81cd8
Maksim Andreev <andreev.kras@gmail.com>
2020-05-29 22:39:09 +0300

    Add 'Mark and Toys' solution

---
+++ problem-solving/greedy-easy-mark-and-toys.js
@@ -0,0 +1,15 @@
+function maximumToys(prices, k) {
+    const sortedPrices = prices.sort((a, b) => a - b);
+    let spendedMoney = 0;
+    let indexAtFinish = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (!(spendedMoney + sortedPrices[i] > k)) {
+            spendedMoney += sortedPrices[i];
+        }
+        else {
+            indexAtFinish = i;
+            break;
+        }
+    }
+    return indexAtFinish;
+}
