4cf7bf76d519cf6fd456bf468d9e9398c21a1783
Maksim Andreev <andreev.kras@gmail.com>
2020-05-29 22:35:19 +0300

    Add 'Sherlock and the Valid String' solution

---
+++ problem-solving/strings-medium-sherlock-and-valid-string.js
@@ -0,0 +1,51 @@
+function isValid(s) {
+    const yes = "YES";
+    const no = "NO";
+    if (s.length === 0) {
+        return no;
+    }
+    if (s.length === 1) {
+        return yes;
+    }
+    const iterableString = [...s];
+    const lettersCounter = {};
+    iterableString.forEach((letter) => {
+        if (!lettersCounter[letter]) {
+            lettersCounter[letter] = 1;
+        }
+        else {
+            lettersCounter[letter] += 1;
+        }
+    });
+    if (Object.keys(lettersCounter).length === 1) {
+        return yes;
+    }
+    const countersFrequency = {};
+    Object.keys(lettersCounter).forEach((key) => {
+        const letter = lettersCounter[key];
+        if (!countersFrequency[letter]) {
+            countersFrequency[letter] = 1;
+        }
+        else {
+            countersFrequency[letter] += 1;
+        }
+    });
+    const counters = Object.keys(countersFrequency).map((freq) => Number(freq));
+    switch (counters.length) {
+        case 1:
+            return yes;
+        case 2:
+            const biggestAmount = Math.max(...counters);
+            const smallestAmount = Math.min(...counters);
+            if (countersFrequency[smallestAmount] === 1 ||
+                (Math.abs(counters[0] - counters[1]) === 1 &&
+                    countersFrequency[biggestAmount] === 1)) {
+                return yes;
+            }
+            else {
+                return no;
+            }
+        default:
+            return no;
+    }
+}
