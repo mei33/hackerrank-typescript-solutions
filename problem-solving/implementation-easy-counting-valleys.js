869b20ff0ab6fde0fc3e2de16169a438260cfae9
Maksim Andreev <andreev.kras@gmail.com>
2020-05-29 22:27:25 +0300

    Add 'Counting Valleys' solution

---
+++ problem-solving/implementation-easy-counting-valleys.js
@@ -0,0 +1,20 @@
+function countingValleys(n, s) {
+    const steps = Array.from(s);
+    let level = 0;
+    let valleysAmount = 0;
+    let isInValley = false;
+    steps.forEach(step => {
+        if (step === 'D') {
+            level -= 1;
+        }
+        else {
+            level += 1;
+        }
+        let isInValleyNow = level < 0;
+        if (isInValleyNow !== isInValley && isInValleyNow) {
+            valleysAmount += 1;
+        }
+        isInValley = isInValleyNow;
+    });
+    return valleysAmount;
+}
---
+++ problem-solving/implementation-easy-counting-valleys.ts
@@ -0,0 +1,24 @@
+function countingValleys(n: number, s: string): number {
+  const steps = Array.from(s);
+  let level = 0;
+  let valleysAmount = 0;
+  let isInValley = false;
+
+  steps.forEach((step) => {
+    if (step === "D") {
+      level -= 1;
+    } else {
+      level += 1;
+    }
+
+    let isInValleyNow = level < 0;
+
+    if (isInValleyNow !== isInValley && isInValleyNow) {
+      valleysAmount += 1;
+    }
+
+    isInValley = isInValleyNow;
+  });
+
+  return valleysAmount;
+}
